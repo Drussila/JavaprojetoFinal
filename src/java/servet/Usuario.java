124307ee87dfbf7dc1db5abaa80e6292998cc1bd
Aluno17 <Aluno17@DESKTOP-UBLU8UF>
2019-10-24 10:29:07 -0300

    criando servet e salvando as informacao numa classe usuario

---
+++ src/java/servet/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servet;
+
+public class Usuario {
+    String nome;
+    String email;
+    String senha;
+    
+}
